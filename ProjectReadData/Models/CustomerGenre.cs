106eaf760290584f015370d7e8fc08c944a914d8
lasse-steinnes <54574913+lasse-steinnes@users.noreply.github.com>
2023-02-20 13:48:19 +0100

    feat: Add class templates for reading data

---
+++ ProjectReadData/Models/CustomerGenre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectReadData.Models
+{
+    internal class CustomerGenre
+    /// <summary>
+    /// Read all the customers in the database, 
+    /// this should display their: Id, first name, last name, country, postal code, 
+    /// phone number and email 
+    /// </summary>
+    {
+
+    }
+}
