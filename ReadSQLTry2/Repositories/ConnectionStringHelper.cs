8926db4a0c66c785994d6bcc71105c540562339f
lasse-steinnes <54574913+lasse-steinnes@users.noreply.github.com>
2023-02-23 08:18:22 +0100

    feat: connectionhelper for Chinook local database

---
+++ ReadSQLTry2/Repositories/ConnectionStringHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadSQLTry2.Repositories
+{
+    public class ConnectionStringHelper
+    {
+        public static string GetConnectionString()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "N-NO-01-01-6706"; // "sqlServerNAMe
+            builder.InitialCatalog = "Chinook"; // database name
+            builder.IntegratedSecurity = true;
+            builder.TrustServerCertificate = true;
+
+            // Check
+            Console.WriteLine(builder.ConnectionString);
+
+            return builder.ConnectionString;
+        }
+    }
+}
