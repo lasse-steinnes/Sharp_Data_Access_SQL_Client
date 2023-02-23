b0401ace661d8a3e912b4cad849213fbcf59ba00
lasse-steinnes <54574913+lasse-steinnes@users.noreply.github.com>
2023-02-23 19:48:59 +0100

    feat: Create CustomerCountry model

---
+++ ReadSQLTry2/Models/CustomerCountry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadSQLTry2.Models
+{
+    public class CustomerCountry
+    {
+        public string Country { get; set; }
+
+        public int Count { get; set; }
+    }
+}
