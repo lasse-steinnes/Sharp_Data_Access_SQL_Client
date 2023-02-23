5b4a269efa797492dc638e8d89f10cfb5662f7cf
lasse-steinnes <54574913+lasse-steinnes@users.noreply.github.com>
2023-02-23 19:49:50 +0100

    feat: Make a member to read and sort countries
    Sorted in descending order

--- ReadSQLTry2/Repositories/CustomerRepository.cs
+++ ReadSQLTry2/Repositories/CustomerRepository.cs
@@ -10,6 +10,9 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 using System.Runtime.Remoting.Messaging;
 using System.Data;
 using System.Drawing;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
 
 namespace ReadSQLTry2.Repositories
 {
@@ -90,45 +93,46 @@ namespace ReadSQLTry2.Repositories
             "CustomerId IS NOT NULL AND FirstName IS NOT NULL AND LastName IS NOT NULL AND Country IS NOT NULL AND " +
             "PostalCode IS NOT NULL AND Phone IS NOT NULL AND Email IS NOT NULL)";
             */
-            try
-            {
-                // Connect
-                using (SqlConnection conn = new SqlConnection(ConnectionStringHelper.GetConnectionString()))
-                {
-                    // open connection
-                    //Console.WriteLine("Here 1");
-                    conn.Open();
-                    //Console.WriteLine("Here2");
-                    // Make a command 
-                    using (SqlCommand cmd = new SqlCommand(sql, conn))
-                    {
-                        // Reader
-                        using (SqlDataReader reader = cmd.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                // Handle result by deserializing
-                                Customer temp = new Customer();
-                                temp.CustomerId = reader.GetInt32(0);
-                                temp.FirstName = reader.GetString(1);
-                                temp.LastName = reader.GetString(2);
-                                temp.Country = reader.GetString(3);
-                                temp.PostalCode = reader.GetString(4);
-                                temp.Phone = reader.GetString(5);
-                                temp.Email = reader.GetString(6);
+        try
+        {
+            // Connect
+            using (SqlConnection conn = new SqlConnection(ConnectionStringHelper.GetConnectionString()))
 
-                                customerSelectionList.Add(temp);
-                                //Console.WriteLine(temp.Email);
-                            }
+            {
+                // open connection
+                //Console.WriteLine("Here 1");
+                conn.Open();
+                //Console.WriteLine("Here2");
+                // Make a command 
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    // Reader
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Handle result by deserializing
+                            Customer temp = new Customer();
+                            temp.CustomerId = reader.GetInt32(0);
+                            temp.FirstName = reader.GetString(1);
+                            temp.LastName = reader.GetString(2);
+                            temp.Country = reader.GetString(3);
+                            temp.PostalCode = reader.GetString(4);
+                            temp.Phone = reader.GetString(5);
+                            temp.Email = reader.GetString(6);
+
+                            customerSelectionList.Add(temp);
+                            //Console.WriteLine(temp.Email);
                         }
                     }
-
                 }
+
             }
-            catch (SqlException ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
+        }
+        catch (SqlException ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
 
 return customerSelectionList;
 }
@@ -278,40 +282,82 @@ return customer;
 }
 
 public bool UpdateCustomer(Customer customer)
-{
-bool success = false;
-string sql = "Update Customer " +
-    "SET CustomerId = @CustomerId, FirstName = @FirstName, LastName = @LastName, " +
-    "Country = @Country, PostalCode = @PostalCode, Phone = @Phone, Email = @Email";
-try
-{
-    using (SqlConnection conn = new SqlConnection(ConnectionStringHelper.GetConnectionString()))
     {
-        conn.Open();
-        using (SqlCommand cmd = new SqlCommand(sql, conn))
+    bool success = false;
+    string sql = "Update Customer " +
+        "SET CustomerId = @CustomerId, FirstName = @FirstName, LastName = @LastName, " +
+        "Country = @Country, PostalCode = @PostalCode, Phone = @Phone, Email = @Email";
+    try
         {
-            // Console.WriteLine(customer);
-            cmd.Parameters.AddWithValue("@CustomerId", customer.CustomerId);
-            cmd.Parameters.AddWithValue("@FirstName", customer.FirstName);
-            cmd.Parameters.AddWithValue("@LastName", customer.LastName);
-            cmd.Parameters.AddWithValue("@Country", customer.Country);
-            cmd.Parameters.AddWithValue("@PostalCode", customer.PostalCode);
-            cmd.Parameters.AddWithValue("@Phone", customer.Phone);
-            cmd.Parameters.AddWithValue("@Email", customer.Email);
-            success = cmd.ExecuteNonQuery() > 0 ? true : false;
+            using (SqlConnection conn = new SqlConnection(ConnectionStringHelper.GetConnectionString()))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    // Console.WriteLine(customer);
+                    cmd.Parameters.AddWithValue("@CustomerId", customer.CustomerId);
+                    cmd.Parameters.AddWithValue("@FirstName", customer.FirstName);
+                    cmd.Parameters.AddWithValue("@LastName", customer.LastName);
+                    cmd.Parameters.AddWithValue("@Country", customer.Country);
+                    cmd.Parameters.AddWithValue("@PostalCode", customer.PostalCode);
+                    cmd.Parameters.AddWithValue("@Phone", customer.Phone);
+                    cmd.Parameters.AddWithValue("@Email", customer.Email);
+                    success = cmd.ExecuteNonQuery() > 0 ? true : false;
+                }
+            }
+        }
+
+        catch (SqlException ex)
+        {
+            // log
+            Console.WriteLine(ex.ToString());
+        }
+        return success;
+
+        }
+
+        public List<CustomerCountry> GetCountriesAndSortByCustomers()
+        {   
+            List<CustomerCountry> countryList = new List<CustomerCountry>();
+            string sql = "SELECT Country, COUNT(CustomerId) FROM Customer GROUP BY Country ORDER BY COUNT(CustomerID) DESC";
+
+            try
+            {
+                // Connect
+                using (SqlConnection conn = new SqlConnection(ConnectionStringHelper.GetConnectionString()))
+
+                {
+                    // open connection
+                    conn.Open();
+                    // Make a command 
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        // Reader
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                // Handle result by deserializing
+                                CustomerCountry temp = new CustomerCountry();
+                                temp.Country = reader.GetString(0);
+                                temp.Count = reader.GetInt32(1);
+                               
+                                countryList.Add(temp);
+                                //Console.WriteLine(temp.Email);
+                            }
+                        }
+                    }
+
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return countryList;
         }
     }
-}
-
-catch (SqlException ex)
-{
-    // log
-    Console.WriteLine(ex.ToString());
-}
-return success;
-
-}
-}
 }
 // NONquery: Insert, update and delete
 // Query: Travese sequential from database
