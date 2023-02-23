4e1945086d29b9fa9513230c493a84307c4fdfe2
lasse-steinnes <54574913+lasse-steinnes@users.noreply.github.com>
2023-02-23 16:02:10 +0100

    feat: fix CustomerRepository interface

---
+++ ReadSQLTry2/Repositories/ICostumerRepository.cs
@@ -0,0 +1,22 @@
+﻿using ReadSQLTry2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadSQLTry2.Repositories
+{
+    // Following the repository design pattern
+    public interface ICustomerRepository
+    { // CRUD: Create, Read, Update, Delete
+        //Customer GetCustomer(int id);
+        List<Customer> GetSelectionCustomers(int limit, int offset);
+        List<Customer> GetAllCustomers();
+        Customer GetCustomerByName(string firstName, string lastName);
+        Customer GetCustomerById(int id);
+        bool UpdateCustomer(Customer customer);
+        bool DeleteCustomer(int id);
+        bool AddNewCustomer(Customer customer);
+    }
+}
