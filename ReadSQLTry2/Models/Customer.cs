0f9c6ef83cbd935d77a736dc2d7bcd13a7c1ae36
lasse-steinnes <54574913+lasse-steinnes@users.noreply.github.com>
2023-02-23 08:17:49 +0100

    feat: model Customer

---
+++ ReadSQLTry2/Models/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace ReadSQLTry2.Models
+{
+    public class Customer
+    { 
+        // reading off customer from SQL database.
+      
+        /*  Read all the customers in the database, 
+        this should display their: Id, first name, last name, country, postal code, 
+        phone number and email.
+      
+        POCO: Plain old C# class object 
+      */
+
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Country { get; set; }
+
+        public string PostalCode {  get; set; }
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+    }
+}
