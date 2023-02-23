38a6c786209c737e7ca557bb875c892fa7ee5ea2
lasse-steinnes <54574913+lasse-steinnes@users.noreply.github.com>
2023-02-23 15:10:31 +0100

    feat: Add GetSelectCustomers

--- ReadSQLTry2/Repositories/CustomerRepository.cs
+++ ReadSQLTry2/Repositories/CustomerRepository.cs
@@ -77,20 +77,23 @@ namespace ReadSQLTry2.Repositories
             return success;
         }
 
-        public List<Customer> GetAllCustomers()
+        public List<Customer> GetSelectionCustomers(int limit, int offset) // NOTE: Cannot get OFFSET to work
         {
-            Console.WriteLine("Get All Customers:");
-            List <Customer> customerList = new List<Customer>();
-            string sql = "SELECT CustomerId, FirstName, LastName, Country, PostalCode, Phone, Email FROM Customer WHERE (" +
-                "CustomerId IS NOT NULL AND FirstName IS NOT NULL AND LastName IS NOT NULL AND Country IS NOT NULL AND " +
-                "PostalCode IS NOT NULL AND Phone IS NOT NULL AND Email IS NOT NULL)";
+            List<Customer> customerSelectionList = new List<Customer>();
+            string sql = $"SELECT TOP {limit} CustomerId, FirstName, LastName, Country, PostalCode, Phone, Email FROM Customer";
+            // $"OFFSET 3 ROWS FETCH NEXT 5 ROWS ONLY";
+            // "TOP 3 OFFSET 3";
+            // TOP {limit} OFFSET {offset}
             // error prone so try catch
             //Console.WriteLine("Here");
+            /* (" +
+            "CustomerId IS NOT NULL AND FirstName IS NOT NULL AND LastName IS NOT NULL AND Country IS NOT NULL AND " +
+            "PostalCode IS NOT NULL AND Phone IS NOT NULL AND Email IS NOT NULL)";
+            */
             try
             {
                 // Connect
                 using (SqlConnection conn = new SqlConnection(ConnectionStringHelper.GetConnectionString()))
-
                 {
                     // open connection
                     //Console.WriteLine("Here 1");
@@ -114,9 +117,8 @@ namespace ReadSQLTry2.Repositories
                                 temp.Phone = reader.GetString(5);
                                 temp.Email = reader.GetString(6);
 
-                                customerList.Add(temp);
+                                customerSelectionList.Add(temp);
                                 //Console.WriteLine(temp.Email);
-          
                             }
                         }
                     }
@@ -128,91 +130,188 @@ namespace ReadSQLTry2.Repositories
                 Console.WriteLine(ex.ToString());
             }
 
-            return customerList;
-        }
+return customerSelectionList;
+}
 
-        public Customer GetCustomer(int id)
-        {   // Write code to get customer by ID 
-            Customer customer = new Customer();
-            string sql = "SELECT CustomerId, FirstName, LastName, Country, PostalCode, Phone, Email FROM Customer WHERE (" +
-                $"CustomerId IS NOT NULL AND CustomerId={id})"; 
-            // AND CustomerId=@CustomerId)";
-            // AND FirstName IS NOT NULL AND LastName IS NOT NULL AND Country IS NOT NULL AND " +
-            //"PostalCode IS NOT NULL AND Phone IS NOT NULL AND Email IS NOT NULL)";
-            // error prone so try catch
-            try
-            {
-                // Connect
-                using (SqlConnection conn = new SqlConnection(ConnectionStringHelper.GetConnectionString()))
+public List<Customer> GetAllCustomers()
+{
+Console.WriteLine("Get All Customers:");
+List <Customer> customerList = new List<Customer>();
+string sql = "SELECT CustomerId, FirstName, LastName, Country, PostalCode, Phone, Email FROM Customer WHERE (" +
+    "CustomerId IS NOT NULL AND FirstName IS NOT NULL AND LastName IS NOT NULL AND Country IS NOT NULL AND " +
+    "PostalCode IS NOT NULL AND Phone IS NOT NULL AND Email IS NOT NULL)";
+// error prone so try catch
+//Console.WriteLine("Here");
+try
+{
+    // Connect
+    using (SqlConnection conn = new SqlConnection(ConnectionStringHelper.GetConnectionString()))
 
-                {
-                    // open connection
-                    //Console.WriteLine("Here 1");
-                    conn.Open();
-                    //Console.WriteLine("Here opened");
-                    // Make a command 
-                    using (SqlCommand cmd = new SqlCommand(sql, conn))
-                    {
-                        // Reader
-                        using (SqlDataReader reader = cmd.ExecuteReader())
-                        {
-                            reader.Read();
-                            // Handle result by deserializing
-                            customer.CustomerId = reader.GetInt32(0);
-                            customer.FirstName = reader.GetString(1);
-                            customer.LastName = reader.GetString(2);
-                            customer.Country = reader.GetString(3);
-                            customer.PostalCode = reader.GetString(4);
-                            customer.Phone = reader.GetString(5);
-                            customer.Email = reader.GetString(6);
-                            Console.WriteLine("Has read");
-                        }
-                    }
-                }
-            }
-            catch (SqlException ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-
-            return customer;
-        }
-
-        public bool UpdateCustomer(Customer customer)
+    {
+        // open connection
+        //Console.WriteLine("Here 1");
+        conn.Open();
+        //Console.WriteLine("Here2");
+        // Make a command 
+        using (SqlCommand cmd = new SqlCommand(sql, conn))
         {
-            bool success = false;
-            string sql = "Update Customer " +
-                "SET CustomerId = @CustomerId, FirstName = @FirstName, LastName = @LastName, " +
-                "Country = @Country, PostalCode = @PostalCode, Phone = @Phone, Email = @Email";
-            try
+            // Reader
+            using (SqlDataReader reader = cmd.ExecuteReader())
             {
-                using (SqlConnection conn = new SqlConnection(ConnectionStringHelper.GetConnectionString()))
+                while (reader.Read())
                 {
-                    conn.Open();
-                    using (SqlCommand cmd = new SqlCommand(sql, conn))
-                    {
-                        // Console.WriteLine(customer);
-                        cmd.Parameters.AddWithValue("@CustomerId", customer.CustomerId);
-                        cmd.Parameters.AddWithValue("@FirstName", customer.FirstName);
-                        cmd.Parameters.AddWithValue("@LastName", customer.LastName);
-                        cmd.Parameters.AddWithValue("@Country", customer.Country);
-                        cmd.Parameters.AddWithValue("@PostalCode", customer.PostalCode);
-                        cmd.Parameters.AddWithValue("@Phone", customer.Phone);
-                        cmd.Parameters.AddWithValue("@Email", customer.Email);
-                        success = cmd.ExecuteNonQuery() > 0 ? true : false;
-                    }
+                    // Handle result by deserializing
+                    Customer temp = new Customer();
+                    temp.CustomerId = reader.GetInt32(0);
+                    temp.FirstName = reader.GetString(1);
+                    temp.LastName = reader.GetString(2);
+                    temp.Country = reader.GetString(3);
+                    temp.PostalCode = reader.GetString(4);
+                    temp.Phone = reader.GetString(5);
+                    temp.Email = reader.GetString(6);
+
+                    customerList.Add(temp);
+                    //Console.WriteLine(temp.Email);
+
                 }
             }
+        }
 
-            catch (SqlException ex)
+    }
+}
+catch (SqlException ex)
+{
+    Console.WriteLine(ex.ToString());
+}
+
+return customerList;
+}
+
+public Customer GetCustomerByName(string firstName, string lastName)
+{   
+// Get by name
+Customer customer = new Customer();
+string sql = "SELECT CustomerId, FirstName, LastName, Country, PostalCode, Phone, Email FROM Customer WHERE (" +
+    $"FirstName LIKE '%{firstName}%' AND LastName LIKE '%{lastName}%' AND CustomerId IS NOT NULL AND Country IS NOT NULL AND " +
+    "PostalCode IS NOT NULL AND Phone IS NOT NULL AND Email IS NOT NULL)";
+// error prone so try catch
+try
+{
+    // Connect
+    using (SqlConnection conn = new SqlConnection(ConnectionStringHelper.GetConnectionString()))
+    {
+        conn.Open();
+
+        // Make a command 
+        using (SqlCommand cmd = new SqlCommand(sql, conn))
+        {
+            // Reader
+            using (SqlDataReader reader = cmd.ExecuteReader())
             {
-                // log
-                Console.WriteLine(ex.ToString());
+                reader.Read();
+                // Handle result by deserializing
+                customer.CustomerId = reader.GetInt32(0);
+                customer.FirstName = reader.GetString(1);
+                customer.LastName = reader.GetString(2);
+                customer.Country = reader.GetString(3);
+                customer.PostalCode = reader.GetString(4);
+                customer.Phone = reader.GetString(5);
+                customer.Email = reader.GetString(6);
+                Console.WriteLine("Has read");
             }
-            return success;
-
         }
     }
+}
+catch (SqlException ex)
+{
+    Console.WriteLine(ex.ToString());
+}
+
+return customer;
+}
+
+public Customer GetCustomerById(int id)
+{   // Write code to get customer by ID 
+Customer customer = new Customer();
+string sql = "SELECT CustomerId, FirstName, LastName, Country, PostalCode, Phone, Email FROM Customer WHERE (" +
+    $"CustomerId={id})"; 
+// AND CustomerId=@CustomerId)";
+// AND FirstName IS NOT NULL AND LastName IS NOT NULL AND Country IS NOT NULL AND " +
+//"PostalCode IS NOT NULL AND Phone IS NOT NULL AND Email IS NOT NULL)";
+// error prone so try catch
+try
+{
+    // Connect
+    using (SqlConnection conn = new SqlConnection(ConnectionStringHelper.GetConnectionString()))
+
+    {
+        // open connection
+        //Console.WriteLine("Here 1");
+        conn.Open();
+        //Console.WriteLine("Here opened");
+        // Make a command 
+        using (SqlCommand cmd = new SqlCommand(sql, conn))
+        {
+            // Reader
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                reader.Read();
+                // Handle result by deserializing
+                customer.CustomerId = reader.GetInt32(0);
+                customer.FirstName = reader.GetString(1);
+                customer.LastName = reader.GetString(2);
+                customer.Country = reader.GetString(3);
+                customer.PostalCode = reader.GetString(4);
+                customer.Phone = reader.GetString(5);
+                customer.Email = reader.GetString(6);
+                Console.WriteLine("Has read");
+            }
+        }
+    }
+}
+catch (SqlException ex)
+{
+    Console.WriteLine(ex.ToString());
+}
+
+return customer;
+}
+
+public bool UpdateCustomer(Customer customer)
+{
+bool success = false;
+string sql = "Update Customer " +
+    "SET CustomerId = @CustomerId, FirstName = @FirstName, LastName = @LastName, " +
+    "Country = @Country, PostalCode = @PostalCode, Phone = @Phone, Email = @Email";
+try
+{
+    using (SqlConnection conn = new SqlConnection(ConnectionStringHelper.GetConnectionString()))
+    {
+        conn.Open();
+        using (SqlCommand cmd = new SqlCommand(sql, conn))
+        {
+            // Console.WriteLine(customer);
+            cmd.Parameters.AddWithValue("@CustomerId", customer.CustomerId);
+            cmd.Parameters.AddWithValue("@FirstName", customer.FirstName);
+            cmd.Parameters.AddWithValue("@LastName", customer.LastName);
+            cmd.Parameters.AddWithValue("@Country", customer.Country);
+            cmd.Parameters.AddWithValue("@PostalCode", customer.PostalCode);
+            cmd.Parameters.AddWithValue("@Phone", customer.Phone);
+            cmd.Parameters.AddWithValue("@Email", customer.Email);
+            success = cmd.ExecuteNonQuery() > 0 ? true : false;
+        }
+    }
+}
+
+catch (SqlException ex)
+{
+    // log
+    Console.WriteLine(ex.ToString());
+}
+return success;
+
+}
+}
 }
 // NONquery: Insert, update and delete
 // Query: Travese sequential from database
