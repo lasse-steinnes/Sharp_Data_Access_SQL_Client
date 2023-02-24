8f1ad75f140b1dd0ebfe14d6bf988087a417b87b
lasse-steinnes <54574913+lasse-steinnes@users.noreply.github.com>
2023-02-24 09:35:15 +0100

    feat: CustomerSpender model

---
+++ ReadSQLTry2/Models/CustomerSpender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadSQLTry2.Models
+{
+    public class CustomerSpender
+    {
+        public int CustomerId { get; set; }
+        public decimal InvoiceTotal { get; set; }
+    }
+}
